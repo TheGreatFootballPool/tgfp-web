1e148893a4b094dc38e07f2042524f6cc08d1016
John Sturgeon <john@sturgeon.me>
2024-08-08 08:04:57 -0700

    Added first Beanie model
    Resolves #19
    Resolves #18

---
+++ app/models.py
@@ -0,0 +1,25 @@
+""" All models for the great football pool """
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import Document, init_beanie
+
+
+# pylint: disable=too-many-ancestors
+class Player(Document):
+    """ Player model """
+    first_name: str
+    last_name: str
+    nick_name: str
+    active: bool
+    email: str
+    discord_id: int
+
+    class Settings:
+        name = "players"
+
+
+async def init():
+    """ Create the client connection"""
+    client = AsyncIOMotorClient("mongodb://tgfp:development@localhost:27017/")
+
+    await init_beanie(database=client.tgfp, document_models=[Player])
