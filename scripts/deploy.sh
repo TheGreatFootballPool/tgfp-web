44d061b311785ee571c0a3181cd60ebd7f6685cf
John Sturgeon <john@sturgeon.me>
2024-08-28 14:49:18 -0700

    Skip prompt for apt install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,9 +8,9 @@ apt update && apt upgrade -y
 if ! dpkg -s python3.11 > /dev/null 2>&1
 then
   echo "TGFP: Installing Python"
-  apt -y install python3.11 python3.11-venv
+  apt install python3.11 python3.11-venv -y
   apt install python3-pip -y
-  apt install python-is-python3
+  apt install python-is-python3 -y
 fi
 
 echo "TGFP: Fetching / installing web site"
@@ -40,7 +40,7 @@ rm ${INSTALL_DIR}/requirements.txt
 if ! dpkg -s 1password-cli > /dev/null 2>&1
 then
   echo "TGFP: Installing 1password-cli"
-  apt install gpg
+  apt install gpg -y
   curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
@@ -51,7 +51,7 @@ then
   mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
   curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-  apt update && apt install 1password-cli
+  apt update && apt install 1password-cli -y
   echo "Installed 1password!"
 fi
 
