27fb0af6136c59cd3884a94860b31d271c6e59f2
John Sturgeon <john@sturgeon.me>
2024-08-30 09:29:38 -0700

    added 'update team record' method

---
+++ app/api/update_team_records.py
@@ -0,0 +1,32 @@
+""" Loop through all the teams, and update their record for the week """
+import asyncio
+from tgfp_nfl import TgfpNfl
+from models import TGFPInfo, get_tgfp_info, Team, db_init
+from config import Config
+
+
+async def update_team_record():
+    """
+    Update all the wins / losses / ties of each team
+    """
+    info: TGFPInfo = await get_tgfp_info()
+    nfl_data_source = TgfpNfl(week_no=info.display_week)
+    for nfl_team in nfl_data_source.teams():
+        team: Team = await Team.find_one(Team.tgfp_nfl_team_id == nfl_team.id)
+        team.wins = nfl_team.wins
+        team.losses = nfl_team.losses
+        team.ties = nfl_team.ties
+        team.logo_url = nfl_team.logo_url
+        # noinspection PyArgumentList
+        team.save()
+
+
+async def main():
+    """ Here so that I can run this manually if I need to """
+    config: Config = Config.get_config()
+    await db_init(config)
+    await update_team_record()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
