c89cf7af37e4fa492c58fd7a47748866227a0f5b
John Sturgeon <john@sturgeon.me>
2024-08-31 19:51:45 -0700

    Fixed (hopefully) the N+1 issue by moving the find out of the loop, and finding once

--- app/api/update_team_records.py
+++ app/api/update_team_records.py
@@ -1,5 +1,7 @@
 """ Loop through all the teams, and update their record for the week """
 import asyncio
+from typing import List
+
 from tgfp_nfl import TgfpNfl
 from models import TGFPInfo, get_tgfp_info, Team, db_init
 from config import Config
@@ -11,14 +13,17 @@ async def update_team_records():
     """
     info: TGFPInfo = await get_tgfp_info()
     nfl_data_source = TgfpNfl(week_no=info.display_week)
+    teams: List[Team] = await Team.find_all().to_list()
     for nfl_team in nfl_data_source.teams():
-        team: Team = await Team.find_one(Team.tgfp_nfl_team_id == nfl_team.id)
-        team.wins = nfl_team.wins
-        team.losses = nfl_team.losses
-        team.ties = nfl_team.ties
-        team.logo_url = nfl_team.logo_url
-        # noinspection PyArgumentList
-        await team.save()
+        for team in teams:
+            if team.tgfp_nfl_team_id == nfl_team.id:
+                team.wins = nfl_team.wins
+                team.losses = nfl_team.losses
+                team.ties = nfl_team.ties
+                team.logo_url = nfl_team.logo_url
+                # noinspection PyArgumentList
+                await team.save()
+                break
 
 
 async def main():
