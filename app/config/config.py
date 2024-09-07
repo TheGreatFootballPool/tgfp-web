827e94797edc60e3e1255491c574bd5e7f05ba97
John Sturgeon <john@sturgeon.me>
2024-09-06 17:25:39 -0700

    Kestra flow scheduling

--- app/config/config.py
+++ app/config/config.py
@@ -13,6 +13,7 @@ class Config:
     ENVIRONMENT: str
     MONGO_URI: str
     WEB_SECRET_KEY: str
+    API_BASE_URL: str
     DISCORD_CLIENT_ID: str
     DISCORD_CLIENT_SECRET: str
     DISCORD_REDIRECT_URI: str
@@ -20,10 +21,8 @@ class Config:
     DISCORD_AUTH_TOKEN: str
     DISCORD_NAG_BOT_CHANNEL_ID: str
     DISCORD_NAG_BOT_WEBHOOK_URL: str
-    MQTT_HOST: str
-    MQTT_TOPIC: str
-    MQTT_USERNAME: str
-    MQTT_PASSWORD: str
+    KESTRA_NAMESPACE: str
+    KESTRA_HOST: str
     SESSION_SECRET_KEY: str
     SENTRY_DSN: str
     APP_VERSION: str
@@ -36,6 +35,7 @@ class Config:
             ENVIRONMENT=os.getenv('ENVIRONMENT'),
             MONGO_URI=os.getenv('MONGO_URI'),
             WEB_SECRET_KEY=os.getenv('WEB_SECRET_KEY'),
+            API_BASE_URL=os.getenv('API_BASE_URL'),
             DISCORD_CLIENT_ID=os.getenv('DISCORD_CLIENT_ID'),
             DISCORD_GUILD_ID=os.getenv('DISCORD_GUILD_ID'),
             DISCORD_CLIENT_SECRET=os.getenv('DISCORD_CLIENT_SECRET'),
@@ -43,11 +43,9 @@ class Config:
             DISCORD_AUTH_TOKEN=os.getenv('DISCORD_AUTH_TOKEN'),
             DISCORD_NAG_BOT_CHANNEL_ID=os.getenv('DISCORD_NAG_BOT_CHANNEL_ID'),
             DISCORD_NAG_BOT_WEBHOOK_URL=os.getenv('DISCORD_NAG_BOT_WEBHOOK_URL'),
-            MQTT_HOST=os.getenv('MQTT_HOST'),
-            MQTT_TOPIC=os.getenv('MQTT_TOPIC'),
-            MQTT_USERNAME=os.getenv('MQTT_USERNAME'),
-            MQTT_PASSWORD=os.getenv('MQTT_PASSWORD'),
             SESSION_SECRET_KEY=os.getenv('SESSION_SECRET_KEY'),
+            KESTRA_NAMESPACE=os.getenv('KESTRA_NAMESPACE'),
+            KESTRA_HOST=os.getenv('KESTRA_HOST'),
             SENTRY_DSN=os.getenv('SENTRY_DSN'),
             APP_VERSION=os.getenv('APP_VERSION')
         )
