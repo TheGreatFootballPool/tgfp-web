c4fc86ae5c13760479e889eb04e45bd303368ed2
John Sturgeon <john@sturgeon.me>
2025-09-07 20:22:52 -0700

    Fixed 502 exception (found by BugSink) (#220)

--- scripts/create_prod_env.sh
+++ scripts/create_prod_env.sh
@@ -7,3 +7,5 @@ echo "ENVIRONMENT=${ENV}" >> "config/.env.${ENV}"
 source config/version.env
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
 echo "APP_VERSION=${NEW_VERSION}" >> "config/.env.${ENV}"
+
+cd ..
