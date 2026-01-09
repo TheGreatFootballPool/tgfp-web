9a483abf10e1cb2ae760957147bcb6084496e1c9
John Sturgeon <john@sturgeon.me>
2026-01-08 20:55:26 -0800

    Manually update the player records when updating all scores manually

--- app/jobs/update_all_scores.py
+++ app/jobs/update_all_scores.py
@@ -3,6 +3,7 @@
 from sqlmodel import Session
 
 from jobs.update_game import _update_one_game
+from jobs.update_player_records import update_player_records
 
 from models import Game
 from models.model_helpers import current_week_info
@@ -11,3 +12,4 @@ from models.model_helpers import current_week_info
 def update_all_scores(session: Session):
     for game in Game.games_for_week(week_info=current_week_info(), session=session):
         _update_one_game(session, game_id=game.id)
+    update_player_records(session=session)
